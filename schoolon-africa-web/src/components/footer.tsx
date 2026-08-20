ece506d33acca8527851a3c910897548d2e15028
theadeptprofessionals <theadeptprofessionals@gmail.com>
2026-08-20 12:07:40 +0100

    Update footer.tsx

--- schoolon-africa-web/src/components/footer.tsx
+++ schoolon-africa-web/src/components/footer.tsx
@@ -171,9 +171,9 @@ export default function Footer() {
                 </a>
               ))}
             </div>
-            <p className="text-[11px] text-slate-400 leading-tight">
+            {/* <p className="text-[11px] text-slate-400 leading-tight">
               Schoolon Africa Portals (Admin, Parent, Mentor, Creator) opening with beta launch.
-            </p>
+            </p> */}
           </div>
 
         </div>
